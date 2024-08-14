5a37f6bd4cc17b47ab9118c483b2b7db0b73d4a3
Prajith-ps <138312306+Prajith-ps@users.noreply.github.com>
2024-08-14 12:20:38 +0530

    Add files via upload

---
+++ _python/assignmentt.py
@@ -0,0 +1,29 @@
+# x=-2
+# if x>0:
+#     print("x is a positive number")
+# elif x==0:
+#     print("x is equal to 0")
+# else:
+#     print("x is a negative number")
+
+
+
+
+# num=int(input("enter a number"))
+# if num>0:
+#       print("number is a positive number")
+# elif num==0:
+#         print("number is zero")
+# else:
+#       print("number is negative")
+
+
+
+x=int(input("enter a number"))
+if x % 2==0:
+    print("x is a even number")
+else:
+    print("x is odd number")
+
+
+
